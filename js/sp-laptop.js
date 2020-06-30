002e69457a9de4542cc916043f16cdb22e90e4f4
DatKhoaiTo <tdat4004@gmail.com>
2020-06-30 20:29:52 +0700

    Di chuyển file JS-laptop vào mục JS

---
+++ js/sp-laptop.js
@@ -0,0 +1,140 @@
+const sanpham = {
+    "alienware m17x": {
+        "name": "Alienware M17 R5",
+        "cpu": "Intel® Core™ i9-8950HK Quad-Core Processor, 2.90GHz x 12, 8MB Intel® Smart Cache, Overclock up to 5.0GHz.",
+        "ram":"32GB DDR4 2666MHz.",
+        "memory":"SSD NVMe 512GB + 01TB HDD",
+        "vga":"nVIDIA GTX 1080 8GB GDDR5",
+        "screen":"17.3 inch QHD (2560 x 1444 pixels) with Tobii IR Eye-tracking, 120Hz.",
+        "keyboard":"RGB",
+        "os":"Windows 10",
+        "color":"Đen",
+        "price":"49.990.000"
+    },
+
+    "ASUS SCAR III": {
+        "name": "ASUS ROG STRIX SCAR III G531G-VES122T ",
+        "cpu": "Intel Core i7-9750H 2.6GHz up to 4.5GHz 12MB",
+        "ram":"16GB (2x 8GB) DDR4 2666MHz (2x SO-DIMM socket, up to 32GB SDRAM)",
+        "memory":"SSD NVMe 512GB",
+        "vga":"NVIDIA GeForce RTX 2060 6GB GDDR6",
+        "screen":"15.6 FHD (1920 x 1080) IPS Non-Glare, NanoEdge, 100% sRGB, 300nits, 144Hz/3ms",
+        "keyboard":"RGB",
+        "os":"Windows 10",
+        "color":"Scar Gunmetal",
+        "price":"45,990,000"
+    },
+
+    "HP Pavilion Gaming ": {
+        "name": "HP Pavilion Gaming 15",
+        "cpu": "Intel Core i7-9750H ( 2.6 GHz - 4.5 GHz / 12MB / 6 nhân, 12 luồng )",
+        "ram":"8GB DDR4 2666MHz ",
+        "memory":"SSD NVMe 512GB + 01TB HDD",
+        "vga":"nVIDIA GeForce GTX 1650 4GB Intel UHD Graphics 630",
+        "keyboard":"LED đơn sắc",
+        "os":"Windows 10",
+        "color":"đen",
+        "price":"27.490.000đ"
+    },
+
+    "Acer Nitro": {
+        "name": "Acer Nitro 5 2020",
+        "cpu": "Intel Core i7 9750H ( 2.6 GHz - 4.5 GHz / 12MB / 6 nhân, 12 luồng )",
+        "ram":"8GB DDR4 2400MHz",
+        "memory":"1TB HDD 7200RPM",
+        "vga":"NVIDIA GeForce GTX 1650 4GB GDDR5 / Intel UHD Graphics 630",
+        "screen":"15.6 inch ( 1920 x 1080 ) Full HD IPS",
+        "keyboard":"LED đơn sắc",
+        "os":"Windows 10",
+        "color":"đen",
+        "price":"28.990.000"
+    },
+
+    "MSI GP62M": {
+        "name": "MSI GP62M 7REX Leopard",
+        "cpu": "Intel Core i7 7700HQ(2.8Ghz, 6MB Cache, Up to 3.8Ghz)",
+        "ram":"16GB(2x8GB)DDR4 2400MHz.",
+        "memory":"128GB NVMe SSD + 1TB (SATA) 7200rpm",
+        "vga":"nVidia GeForce GTX1050Ti 4GB GDDR5",
+        "screen":"15.6 inch Full HD (1920x1080), 45%NTSC color Anti-glare",
+        "keyboard":"RGB",
+        "os":"Free DOS",
+        "color":"đen",
+        "price":"26.990.000"
+    },
+
+    "lenovo Y540": {
+        "name": "Lenovo Legion Y540",
+        "cpu": "Intel® Core™ i7-9750H(2.60GHz upto 4.50GHz, 12MB)",
+        "ram":"8GB DDR4 2666MHz",
+        "memory":"128GB SSD M.2 2242 NVMe + 1TB HDD",
+        "vga":"nVidia Geforce GTX 1650 4GB DDR5",
+        "screen":"15.6 inch FHD (1920*1080) IPS",
+        "keyboard":"RGB",
+        "os":"Windows 10",
+        "color":"đen",
+        "price":"26.990.000 "
+    },
+
+    "Asus Vivobook X409": {
+        "name": "Asus Vivobook X409MA",
+        "cpu": "Intel Celeron Processor N4020 (1.10GHz upto 2.80GHz, 4MB)",
+        "ram":"4GB(1 x 4GB DDR4) 2400MHz",
+        "memory":"256GB SSD",
+        "vga":"Intel UHD Graphics 620",
+        "screen":"14.0 inch (16:9) LED-backlit HD (1366x768) 60Hz",
+        "keyboard":"Không LED",
+        "os":"Windows 10",
+        "color":"Bạc",
+        "price":"6.990.000"
+    },
+    "Lenovo IdeaPad S145": {
+        "name": "Lenovo IdeaPad S145-15API 81UT00DMVN",
+        "cpu": "AMD Ryzen™ 3 3200U (2.60Ghz upto 3.50Ghz, 4MB)",
+        "ram":"4GB(1 x 4GB DDR4) 2400MHz",
+        "memory":"256GB SSD M.2 2242 NVMe",
+        "vga":"Radeon™ Vega 3 Graphics",
+        "screen":"14.0 inch FHD (1920x1080) TN 220nits Anti-glare",
+        "keyboard":"Không LED",
+        "os":"Windows 10",
+        "color":"Xám",
+        "price":"8.990.000"
+    },
+     "Acer Aspire ": {
+        "name": "Acer Aspire A515",
+        "cpu": "Intel Core i5-8265U (1.60 upto 3.90GHz, 4 nhân 8 luồng, 6MB)",
+        "ram":"4GB(1 x 4GB DDR4) 2400MHz",
+        "memory":"16GB M.2 Intel Optane + 1TB 5400rpm",
+        "vga":"Intel UHD Graphics 620",
+        "screen":"15.6 inch FHD (1920x1080)",
+        "keyboard":"Không LED",
+        "os":"Free DOS",
+        "color":"Bạc",
+        "price":"10.790.000"
+    }, 
+    "Dell 3490": {
+        "name": "Dell Vostro 3490",
+        "cpu": "Intel Core™ i3-10110U (2.10 GHz upto 4.10 GHz, 4MB))",
+        "ram":"4GB(1 x 4GB DDR4) 2400MHz",
+        "memory":"256GB SSD M.2 PCIe NVMe",
+        "vga":"ntel® UHD Graphics",
+        "screen":"14.0 inch FHD (1920 x 1080",
+        "keyboard":"Không LED",
+        "os":"Windows 10",
+        "color":"Đen",
+        "price":"10.790.000"
+    }, 
+    "HP 15s": {
+        "name": "HP 15s-fq1106TU",
+        "cpu": "Intel® Core i3-1005G1 (1.20GHz upto 3.40GHz, 4MB)",
+        "ram":"4GB(1 x 4GB DDR4) 2400MHz",
+        "memory":"256GB PCIe® NVMe™ M.2 SSD",
+        "vga":"Intel® UHD Graphics",
+        "screen":"14.0 inch (16:9) LED-backlit HD (1366x768) 60Hz",
+        "keyboard":"Không LED",
+        "os":"Windows 10",
+        "color":"Bạc",
+        "price":"9.990.000"
+    }
+
+}
